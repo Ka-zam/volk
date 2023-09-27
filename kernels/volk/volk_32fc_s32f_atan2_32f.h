2e5cbe399c636480affc86e64b8eeef038f744f3
Magnus Lundmark <magnuslundmark@gmail.com>
2023-09-27 16:52:02 +0200

    initial commit
    Signed-off-by: Magnus Lundmark <magnuslundmark@gmail.com>

--- kernels/volk/volk_32fc_s32f_atan2_32f.h
+++ kernels/volk/volk_32fc_s32f_atan2_32f.h
@@ -65,6 +65,60 @@
 #include <math.h>
 #include <stdio.h>
 
+#if LV_HAVE_AVX2 && LV_HAVE_FMA
+#include <immintrin.h>
+#include <volk/volk_avx2_fma_intrinsics.h>
+static inline void volk_32fc_s32f_atan2_32f_a_avx2_fma(float* outputVector,
+                                                     const lv_32fc_t* complexVector,
+                                                     const float normalizeFactor,
+                                                     unsigned int num_points) {
+    const float* in = (float*) complexVector;
+     float* out = (float*) outputVector;
+    const float invNormalizeFactor = 1.0 / normalizeFactor;     
+  const __m256 vinvNormalizeFactor = _mm256_set1_ps(invNormalizeFactor);
+  const __m256 pi = _mm256_set1_ps(0x1.921fb6p1f); // pi
+  const __m256 pi_2 = _mm256_set1_ps(0x1.921fb6p0f); //pi / 2
+
+  const __m256 abs_mask = _mm256_castsi256_ps(_mm256_set1_epi32(0x7FFFFFFF));;
+  const __m256 sign_mask = _mm256_castsi256_ps(_mm256_set1_epi32(0x80000000));   
+
+    unsigned int number = 0;
+    unsigned int eighth_points = num_points / 8;
+    for (; number < eighth_points; number++) {   
+        __m256 z1 = _mm256_load_ps(in);
+        in += 8;
+        __m256 z2 = _mm256_load_ps(in);
+        in += 8;
+
+        __m256 x = _mm256_shuffle_ps(z1, z2, _MM_SHUFFLE(2, 0, 2, 0));
+        __m256 y = _mm256_shuffle_ps(z1, z2, _MM_SHUFFLE(3, 1, 3, 1));
+
+        __m256 swap_mask = _mm256_cmp_ps(
+            _mm256_and_ps(y, abs_mask),
+            _mm256_and_ps(x, abs_mask), 
+            _CMP_GT_OS);  
+        __m256 input = _mm256_div_ps(
+            _mm256_blendv_ps(y, x, swap_mask), 
+            _mm256_blendv_ps(x, y, swap_mask) );
+        __m256 result = _m256_arctan_approximation_avx2_fma(input);
+        result = _mm256_blendv_ps( result, _mm256_sub_ps( _mm256_or_ps(pi_2, _mm256_and_ps(input, sign_mask)), result ), swap_mask );
+        __m256 x_sign_mask = _mm256_castsi256_ps(_mm256_srai_epi32(_mm256_castps_si256(x), 31));
+        result = _mm256_add_ps(_mm256_and_ps(_mm256_xor_ps(pi, _mm256_and_ps(sign_mask, y)), x_sign_mask), result);
+        result = _mm256_mul_ps(result, vinvNormalizeFactor);
+        _mm256_store_ps(out, result);
+        out += 8;
+    }
+
+    number = eighth_points * 8;
+    for (; number < num_points; number++) {
+        const float x = *in++;
+        const float y = *in++;
+        *out++ = atan2f(y, x) * invNormalizeFactor;
+    }    
+
+}
+#endif /* LV_HAVE_AVX2 && LV_HAVE_FMA for aligned */
+
 #ifdef LV_HAVE_SSE4_1
 #include <smmintrin.h>
 
