19e419ffefab2d81ae96f996e58bd500228f2f51
Magnus Lundmark <magnuslundmark@gmail.com>
2023-09-28 17:09:53 +0200

    wip
    Signed-off-by: Magnus Lundmark <magnuslundmark@gmail.com>

--- kernels/volk/volk_32fc_s32f_atan2_32f.h
+++ kernels/volk/volk_32fc_s32f_atan2_32f.h
@@ -1,6 +1,7 @@
 /* -*- c++ -*- */
 /*
  * Copyright 2012, 2014 Free Software Foundation, Inc.
+ * Copyright 2023 Magnus Lundmark <magnuslundmark@gmail.com>
  *
  * This file is part of VOLK
  *
@@ -73,14 +74,15 @@ static inline void volk_32fc_s32f_atan2_32f_a_avx2_fma(float* outputVector,
                                                      const float normalizeFactor,
                                                      unsigned int num_points) {
     const float* in = (float*) complexVector;
-     float* out = (float*) outputVector;
-    const float invNormalizeFactor = 1.0 / normalizeFactor;     
-  const __m256 vinvNormalizeFactor = _mm256_set1_ps(invNormalizeFactor);
-  const __m256 pi = _mm256_set1_ps(0x1.921fb6p1f); // pi
-  const __m256 pi_2 = _mm256_set1_ps(0x1.921fb6p0f); //pi / 2
-
-  const __m256 abs_mask = _mm256_castsi256_ps(_mm256_set1_epi32(0x7FFFFFFF));;
-  const __m256 sign_mask = _mm256_castsi256_ps(_mm256_set1_epi32(0x80000000));   
+    float* out = (float*) outputVector;
+    
+    const float invNormalizeFactor = 1.f / normalizeFactor;     
+    const __m256 vinvNormalizeFactor = _mm256_set1_ps(invNormalizeFactor);
+    const __m256 pi = _mm256_set1_ps(0x1.921fb6p1f);    // pi
+    const __m256 pi_2 = _mm256_set1_ps(0x1.921fb6p0f);  // pi / 2
+    const __m256i permute_mask = _mm256_set_epi32(7, 6, 3, 2, 5, 4, 1, 0);
+    const __m256 abs_mask = _mm256_castsi256_ps(_mm256_set1_epi32(0x7FFFFFFF));;
+    const __m256 sign_mask = _mm256_castsi256_ps(_mm256_set1_epi32(0x80000000));   
 
     unsigned int number = 0;
     unsigned int eighth_points = num_points / 8;
@@ -91,20 +93,26 @@ static inline void volk_32fc_s32f_atan2_32f_a_avx2_fma(float* outputVector,
         in += 8;
 
         __m256 x = _mm256_shuffle_ps(z1, z2, _MM_SHUFFLE(2, 0, 2, 0));
+        x = _mm256_permutevar8x32_ps(x, permute_mask);
         __m256 y = _mm256_shuffle_ps(z1, z2, _MM_SHUFFLE(3, 1, 3, 1));
+        y = _mm256_permutevar8x32_ps(y, permute_mask);
 
-        __m256 swap_mask = _mm256_cmp_ps(
-            _mm256_and_ps(y, abs_mask),
-            _mm256_and_ps(x, abs_mask), 
-            _CMP_GT_OS);  
-        __m256 input = _mm256_div_ps(
-            _mm256_blendv_ps(y, x, swap_mask), 
-            _mm256_blendv_ps(x, y, swap_mask) );
+        __m256 swap_mask = _mm256_cmp_ps(_mm256_and_ps(y, abs_mask), _mm256_and_ps(x, abs_mask), _CMP_GT_OS);
+
+        __m256 input = _mm256_div_ps(_mm256_blendv_ps(y, x, swap_mask), _mm256_blendv_ps(x, y, swap_mask) );
+
         __m256 result = _m256_arctan_approximation_avx2_fma(input);
-        result = _mm256_blendv_ps( result, _mm256_sub_ps( _mm256_or_ps(pi_2, _mm256_and_ps(input, sign_mask)), result ), swap_mask );
+
+        input = _mm256_sub_ps( _mm256_or_ps(pi_2, _mm256_and_ps(input, sign_mask)), result );
+        result = _mm256_blendv_ps( result, input, swap_mask );
+
         __m256 x_sign_mask = _mm256_castsi256_ps(_mm256_srai_epi32(_mm256_castps_si256(x), 31));
+        float a[8];
+        printf("xsm: %f %f %f %f %f %f %f %f\n", a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7]);
+
         result = _mm256_add_ps(_mm256_and_ps(_mm256_xor_ps(pi, _mm256_and_ps(sign_mask, y)), x_sign_mask), result);
         result = _mm256_mul_ps(result, vinvNormalizeFactor);
+
         _mm256_store_ps(out, result);
         out += 8;
     }
@@ -119,125 +127,72 @@ static inline void volk_32fc_s32f_atan2_32f_a_avx2_fma(float* outputVector,
 }
 #endif /* LV_HAVE_AVX2 && LV_HAVE_FMA for aligned */
 
-#ifdef LV_HAVE_SSE4_1
-#include <smmintrin.h>
-
-#ifdef LV_HAVE_LIB_SIMDMATH
-#include <simdmath.h>
-#endif /* LV_HAVE_LIB_SIMDMATH */
-
-static inline void volk_32fc_s32f_atan2_32f_a_sse4_1(float* outputVector,
+#if LV_HAVE_AVX
+#include <immintrin.h>
+#include <volk/volk_avx2_fma_intrinsics.h>
+static inline void volk_32fc_s32f_atan2_32f_a_avx(float* outputVector,
                                                      const lv_32fc_t* complexVector,
                                                      const float normalizeFactor,
-                                                     unsigned int num_points)
-{
-    const float* complexVectorPtr = (float*)complexVector;
-    float* outPtr = outputVector;
+                                                     unsigned int num_points) {
+    const float* in = (float*) complexVector;
+    float* out = (float*) outputVector;
+    
+    const float invNormalizeFactor = 1.f / normalizeFactor;     
+    const __m256 vinvNormalizeFactor = _mm256_set1_ps(invNormalizeFactor);
+    //const __m256 zero  = _mm256_set1_ps(0.f);    // pi
+    const __m256 pi = _mm256_set1_ps(0x1.921fb6p1f);    // pi
+    const __m256 pi_2 = _mm256_set1_ps(0x1.921fb6p0f);  // pi / 2
+    const __m256i permute_mask = _mm256_set_epi32(7, 6, 3, 2, 5, 4, 1, 0);
+    const __m256 abs_mask = _mm256_castsi256_ps(_mm256_set1_epi32(0x7FFFFFFF));;
+    const __m256 sign_mask = _mm256_castsi256_ps(_mm256_set1_epi32(0x80000000));   
 
     unsigned int number = 0;
-    const float invNormalizeFactor = 1.0 / normalizeFactor;
+    unsigned int eighth_points = num_points / 8;
+    for (; number < eighth_points; number++) {   
+        __m256 z1 = _mm256_load_ps(in);
+        in += 8;
+        __m256 z2 = _mm256_load_ps(in);
+        in += 8;
 
-#ifdef LV_HAVE_LIB_SIMDMATH
-    const unsigned int quarterPoints = num_points / 4;
-    __m128 testVector = _mm_set_ps1(2 * M_PI);
-    __m128 correctVector = _mm_set_ps1(M_PI);
-    __m128 vNormalizeFactor = _mm_set_ps1(invNormalizeFactor);
-    __m128 phase;
-    __m128 complex1, complex2, iValue, qValue;
-    __m128 keepMask;
+        __m256 x = _mm256_shuffle_ps(z1, z2, _MM_SHUFFLE(2, 0, 2, 0));
+        x = _mm256_permutevar_ps(x, permute_mask);
+        __m256 y = _mm256_shuffle_ps(z1, z2, _MM_SHUFFLE(3, 1, 3, 1));
+        y = _mm256_permutevar_ps(y, permute_mask);
 
-    for (; number < quarterPoints; number++) {
-        // Load IQ data:
-        complex1 = _mm_load_ps(complexVectorPtr);
-        complexVectorPtr += 4;
-        complex2 = _mm_load_ps(complexVectorPtr);
-        complexVectorPtr += 4;
-        // Deinterleave IQ data:
-        iValue = _mm_shuffle_ps(complex1, complex2, _MM_SHUFFLE(2, 0, 2, 0));
-        qValue = _mm_shuffle_ps(complex1, complex2, _MM_SHUFFLE(3, 1, 3, 1));
-        // Arctan to get phase:
-        phase = atan2f4(qValue, iValue);
-        // When Q = 0 and I < 0, atan2f4 sucks and returns 2pi vice pi.
-        // Compare to 2pi:
-        keepMask = _mm_cmpneq_ps(phase, testVector);
-        phase = _mm_blendv_ps(correctVector, phase, keepMask);
-        // done with above correction.
-        phase = _mm_mul_ps(phase, vNormalizeFactor);
-        _mm_store_ps((float*)outPtr, phase);
-        outPtr += 4;
+        __m256 swap_mask = _mm256_cmp_ps(_mm256_and_ps(y, abs_mask), _mm256_and_ps(x, abs_mask), _CMP_GT_OS);  
+        __m256 input = _mm256_div_ps(_mm256_blendv_ps(y, x, swap_mask), _mm256_blendv_ps(x, y, swap_mask) );
+
+        __m256 result = _m256_arctan_approximation_avx(input);
+
+        input = _mm256_sub_ps( _mm256_or_ps(pi_2, _mm256_and_ps(input, sign_mask)), result );
+        result = _mm256_blendv_ps( result, input, swap_mask );
+
+        __m128 x_l = _mm256_extractf128_ps(x, 1);
+        __m128 x_h = _mm256_extractf128_ps(x, 0);
+        x_l = _mm_castsi128_ps(_mm_srai_epi32(_mm_castps_si128(x_l), 31));
+        x_h = _mm_castsi128_ps(_mm_srai_epi32(_mm_castps_si128(x_h), 31));
+        __m256 x_sign_mask = _mm256_set_m128(x_h, x_l);
+
+
+        //__m256 x_sign_mask = _mm256_srai_epi32(_mm256_castps_si256(x), 31)
+        //x_sign_mask = _mm256_castsi256_ps(x_sign_mask);
+
+        result = _mm256_add_ps(_mm256_and_ps(_mm256_xor_ps(pi, _mm256_and_ps(sign_mask, y)), x_sign_mask), result);
+        result = _mm256_mul_ps(result, vinvNormalizeFactor);
+
+        _mm256_store_ps(out, result);
+        out += 8;
     }
-    number = quarterPoints * 4;
-#endif /* LV_HAVE_LIB_SIMDMATH */
 
+    number = eighth_points * 8;
     for (; number < num_points; number++) {
-        const float real = *complexVectorPtr++;
-        const float imag = *complexVectorPtr++;
-        *outPtr++ = atan2f(imag, real) * invNormalizeFactor;
-    }
+        const float x = *in++;
+        const float y = *in++;
+        *out++ = atan2f(y, x) * invNormalizeFactor;
+    }    
+
 }
-#endif /* LV_HAVE_SSE4_1 */
-
-
-#ifdef LV_HAVE_SSE
-#include <xmmintrin.h>
-
-#ifdef LV_HAVE_LIB_SIMDMATH
-#include <simdmath.h>
-#endif /* LV_HAVE_LIB_SIMDMATH */
-
-static inline void volk_32fc_s32f_atan2_32f_a_sse(float* outputVector,
-                                                  const lv_32fc_t* complexVector,
-                                                  const float normalizeFactor,
-                                                  unsigned int num_points)
-{
-    const float* complexVectorPtr = (float*)complexVector;
-    float* outPtr = outputVector;
-
-    unsigned int number = 0;
-    const float invNormalizeFactor = 1.0 / normalizeFactor;
-
-#ifdef LV_HAVE_LIB_SIMDMATH
-    const unsigned int quarterPoints = num_points / 4;
-    __m128 testVector = _mm_set_ps1(2 * M_PI);
-    __m128 correctVector = _mm_set_ps1(M_PI);
-    __m128 vNormalizeFactor = _mm_set_ps1(invNormalizeFactor);
-    __m128 phase;
-    __m128 complex1, complex2, iValue, qValue;
-    __m128 mask;
-    __m128 keepMask;
-
-    for (; number < quarterPoints; number++) {
-        // Load IQ data:
-        complex1 = _mm_load_ps(complexVectorPtr);
-        complexVectorPtr += 4;
-        complex2 = _mm_load_ps(complexVectorPtr);
-        complexVectorPtr += 4;
-        // Deinterleave IQ data:
-        iValue = _mm_shuffle_ps(complex1, complex2, _MM_SHUFFLE(2, 0, 2, 0));
-        qValue = _mm_shuffle_ps(complex1, complex2, _MM_SHUFFLE(3, 1, 3, 1));
-        // Arctan to get phase:
-        phase = atan2f4(qValue, iValue);
-        // When Q = 0 and I < 0, atan2f4 sucks and returns 2pi vice pi.
-        // Compare to 2pi:
-        keepMask = _mm_cmpneq_ps(phase, testVector);
-        phase = _mm_and_ps(phase, keepMask);
-        mask = _mm_andnot_ps(keepMask, correctVector);
-        phase = _mm_or_ps(phase, mask);
-        // done with above correction.
-        phase = _mm_mul_ps(phase, vNormalizeFactor);
-        _mm_store_ps((float*)outPtr, phase);
-        outPtr += 4;
-    }
-    number = quarterPoints * 4;
-#endif /* LV_HAVE_LIB_SIMDMATH */
-
-    for (; number < num_points; number++) {
-        const float real = *complexVectorPtr++;
-        const float imag = *complexVectorPtr++;
-        *outPtr++ = atan2f(imag, real) * invNormalizeFactor;
-    }
-}
-#endif /* LV_HAVE_SSE */
+#endif /* LV_HAVE_AVX for aligned */
 
 #ifdef LV_HAVE_GENERIC
 
